c198556e1a595be449b98c72e022599b80ef043a
ktunchar <maikittitee1615@gmail.com>
2024-09-23 01:11:00 +0700

    feat: login and register using 42OAuth

--- User_manage/Authentication/utils.py
+++ User_manage/Authentication/utils.py
@@ -16,15 +16,18 @@ def fetch_42user_data(token: str):
 		print("No Token")
 		return
 	url = 'https://api.intra.42.fr/v2/me'
+	print(f"token: {token}")
 	headers = {
 		"accept": "application/json",
- 		'Authorization': "Bearer 04ccce351e0cf53a4390358ae7dc66277572cfc4216c4a63021d6cca072c8298",
+ 		# 'Authorization': "Bearer dfcec24be3e42193e7f17b0593f825ea9058c5c136450403e9532feb60df6ee0",
+ 		'Authorization': f"Bearer {token}",
 	}
-	# print(f"token: {token}")
 	response = requests.get(url, headers=headers)
-	print(dict(response.json()))
+	data = response.json()
+	print(f"data: {data}")
+	try:
+		return ({"message": data['error']})
+	except:
+		return (response.json())
 
 
-fetch_42user_data("Dsd")
-# def get_user(dict: json):
-# 	# print(user)
