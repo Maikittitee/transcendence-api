6c1d0776e4c51c670e571720bdddd925ba75b73c
nkietwee <nawaporn.kiet@gmail.com>
2024-09-26 17:31:39 +0700

    hash password

--- User_manage/User_manage/settings.py
+++ User_manage/User_manage/settings.py
@@ -82,14 +82,23 @@ WSGI_APPLICATION = "User_manage.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb_tran', 
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
