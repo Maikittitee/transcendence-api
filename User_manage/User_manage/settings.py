bdfdd37a52830c62413b163b6630208816b81ae4
nkietwee <nawaporn.kiet@gmail.com>
2024-10-05 19:08:14 +0700

    add user postgres docker

--- User_manage/User_manage/settings.py
+++ User_manage/User_manage/settings.py
@@ -84,27 +84,25 @@ WSGI_APPLICATION = "User_manage.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db_user', 
         'USER': 'postgres',
         'PASSWORD': '1',
-        'HOST': 'localhost', 
+        'HOST': 'postgres', 
         'PORT': '5432',
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB', 'db_user'),
-#         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', '1'),
-#         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-#         'PORT': '5432',
-#     }
-# }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
--- data/start.sh
+++ data/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh 
 
-python3 User_manage/manage.py makemigrations --no-input
+python3 User_manage/manage.py makemigrations --merge --no-input
 python3 User_manage/manage.py migrate --no-input
 python3 User_manage/manage.py runserver 0.0.0.0:8001
