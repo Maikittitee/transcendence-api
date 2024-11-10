ff52abd947e154e083e8e48aa7a148e118a9b4b1
ktunchar <maikittitee1615@gmail.com>
2024-11-10 21:12:46 +0700

    docs: swagger at localhost:9000/docs

--- User_manage/User_manage/settings.py
+++ User_manage/User_manage/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'corsheaders',
     'rest_framework',
+	'drf_yasg',
 ]
 
 
--- User_manage/User_manage/urls.py
+++ User_manage/User_manage/urls.py
@@ -15,12 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.urls import path, include
+from django.contrib import admin
+
+# Schema view configuration
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourdomain.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("Account.urls")),
     path("auth/", include("Authentication.urls")),
-	path("profile/", include("Profile.urls"))
+	path("profile/", include("Profile.urls")),
+	path('docs/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='swagger-docs'),
 ]
