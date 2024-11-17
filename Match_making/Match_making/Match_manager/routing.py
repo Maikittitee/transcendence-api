de9145981ff791e9069c2ed04bbae610528a23e1
EsperSaN <polawat.ruenruay@hotmail.com>
2024-11-17 19:48:03 +0700

    add the ws:// path route

--- Match_making/Match_making/Match_manager/routing.py
+++ Match_making/Match_making/Match_manager/routing.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .consumers import *
 
-websocket_urlpatterns = [{
+websocket_urlpatterns = [
     #queue routes
     path('ws/matchmaking/', MatchmakingConsumer.as_asgi()),
     
@@ -9,10 +9,10 @@ websocket_urlpatterns = [{
     path('ws/tournament/create/', TournamentConsumer.as_asgi()),
     path('ws/tournament/<str:tournament_id>/', TournamentConsumer.as_asgi()),
     
-    # Game routes
-    path('ws/game/create/', GameConsumer.as_asgi()),
-    path('ws/game/<str:game_id>/', GameConsumer.as_asgi()),
+    # # Game routes
+    # path('ws/game/create/', GameConsumer.as_asgi()),
+    # path('ws/game/<str:game_id>/', GameConsumer.as_asgi()),
     
-    # Spectator routes
-    path('ws/spectate/<str:game_id>/', SpectatorConsumer.as_asgi()),
-}]
+    # # Spectator routes
+    # path('ws/spectate/<str:game_id>/', SpectatorConsumer.as_asgi()),
+]
