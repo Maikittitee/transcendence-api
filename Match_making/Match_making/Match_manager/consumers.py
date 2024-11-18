d05472c413182ff6618ffb70a7546211f028de1b
EsperSaN <polawat.ruenruay@hotmail.com>
2024-11-18 19:13:23 +0700

    change the send methot to 'await' i an idiot

--- Match_making/Match_making/Match_manager/consumers.py
+++ Match_making/Match_making/Match_manager/consumers.py
@@ -4,26 +4,29 @@ class MatchmakingConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.accept()
         print("MatchmakingConsumer Connected")
-        self.send("MatchmakingConsumer Connected")
+        await self.send("MatchmakingConsumer Connected")
 
     async def disconnect(self, close_code):
         print("MatchmakingConsumer Disconnected")
     async def receive(self, text_data=None):
         print("TournamentConsumer Received", text_data)   
+        await self.send(text_data); 
     # async def receive_json(self, content):
     #     print("MatchmakingConsumer Received", content)
-    #     await self.send_json(content)
+    #     await await self.send_json(content)
 
 class TournamentConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.accept()
         print("TournamentConsumer Connected")
-        self.send("TournamentConsumer Connected")
+        await self.send("TournamentConsumer Connected")
 
     async def disconnect(self, close_code):
         print("TournamentConsumer Disconnected")
+
     async def receive(self, text_data=None):
-        print("TournamentConsumer Received", text_data)   
+        print("TournamentConsumer Received", text_data)  
+        await self.send(text_data); 
     # async def receive_json(self,  content):
     #     print("TournamentConsumer Received", content)
-    #     await self.send_json(content)
+    #     await await self.send_json(content)
