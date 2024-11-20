734aae138764cc43582817689ddd1a3689abec06
ktunchar <maikittitee1615@gmail.com>
2024-11-20 22:47:43 +0700

    feat: initial profile model

--- User_manage/Profile/models.py
+++ User_manage/Profile/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+from Account.models import User
 # Create your models here.
 
-class UserProfile():
-	pass
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+	image_location = models.CharField(default="images/default.jpg")
+	win = models.IntegerField()
+	loss = models.IntegerField()
+	total_match = models.IntegerField()
+
+	def __str__(self):
+		return f"{self.user.username}'s Profile"
