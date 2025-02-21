43ccfe50acf38793c3e86ff2a150e2072e6ea00f
ktunchar <maikittitee1615@gmail.com>
2025-02-21 22:09:47 +0700

    fix: db

--- backend/Match_making/wait-for-db.sh
+++ backend/Match_making/wait-for-db.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "root" -d "db1" -c '\q'; do
+# Use environment variables from .env
+until PGPASSWORD="${DB_PASSWORD}" psql -h "db" -U "${DB_USER}" -d "${DB_NAME}" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "Postgres is up - executing command"
+
 python3 Match_making/manage.py migrate --noinput
 
 # Start server
